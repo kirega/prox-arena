2b3177db4b4c0acb5245544090ceaf84fe2f489c
kirega <joseph.mutiga@gmail.com>
2020-04-05 01:27:52 +0300

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'team-battles';
+  userNames = [];
+  user;
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit(): void {
+    const headers: HttpHeaders = new HttpHeaders({
+      Accept: 'application/vnd.lichess.v2+json',
+    })
+    this.http.get('https://cors-anywhere.herokuapp.com/https://lichess.org/@/kirega/perf/blitz', { headers })
+      .subscribe(
+        (res: any) => {
+          console.log(res);
+          console.log(res.stat.highest.int);
+        }
+      );
+  }
 }
