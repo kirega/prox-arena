cc2bcc38c59e042cec08a729bf78816baf6685f4
kirega <joseph.mutiga@gmail.com>
2020-04-10 16:15:40 +0300

    fixes directive error

--- src/app/directives/auth.directive.ts
+++ src/app/directives/auth.directive.ts
@@ -11,28 +11,27 @@ export class AuthDirective {
     private viewContainer: ViewContainerRef
   ) { }
 
-  @Input() set appAuth(roles: string){
+  @Input() set appAuth(roles: string) {
     const token = JSON.parse(localStorage.getItem('token'));
     let user;
     if (token) {
       user = jwt_decode(token.accessToken);
-      if (roles){
-        if (roles === user.permission && !this.show) {
-          // show the component
-          this.viewContainer.createEmbeddedView(this.templateRef);
-          this.show = true;
-        } else if (!(roles === user.permission)  && this.show){
-          // remove the component
-          this.viewContainer.clear();
-          this.show = false;
-        }
-      } else {
+      if (user && !this.show) {
+        // if (roles === user.permission && !this.show) {
+        // show the component
         this.viewContainer.createEmbeddedView(this.templateRef);
         this.show = true;
+      } else if (!user && this.show) {
+        // remove the component
+        this.viewContainer.clear();
+        this.show = false;
       }
     }
-    // Check if the current user has the correct access rights needed for that action
-    // the access control also works for user ids
+    else {
+      this.viewContainer.createEmbeddedView(this.templateRef);
+      this.show = true;
+    }
   }
-
+  // Check if the current user has the correct access rights needed for that action
+  // the access control also works for user ids
 }
