f075520f829bbd5d7d0d5f567d1c918f6386fd1d
kirega <joseph.mutiga@gmail.com>
2020-04-18 13:35:30 +0300

    Updated the migration

--- api/migrations/20200410102154-addedAssociations.js
+++ api/migrations/20200410102154-addedAssociations.js
@@ -9,28 +9,29 @@ module.exports = {
       Example:
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
-    // return queryInterface.addColumn(
-    //   'Users', // name of Source model
-    //   'teamId', // name of the key we're adding 
-    //   {
-    //     type: Sequelize.INTEGER,
-    //     references: {
-    //       model: 'teams', // name of Target model
-    //       key: 'id', // key in Target model that we're referencing
-    //     },
-    //     onUpdate: 'CASCADE',
-    //     onDelete: 'CASCADE',
-    //   }
-    //   );
-      return queryInterface.addColumn(
-        'Users', // name of Source model
-        'paymentStatus', // name of the key we're adding 
-        {
-          type: Sequelize.BOOLEAN,
-          defaultValue: false 
-        }
-        );
-      
+    return queryInterface.addColumn(
+      'Users', // name of Source model
+      'teamId', // name of the key we're adding 
+      {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'teams', // name of Target model
+          key: 'id', // key in Target model that we're referencing
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      }
+      ).then( () => {
+        return queryInterface.addColumn(
+          'Users', // name of Source model
+          'paymentStatus', // name of the key we're adding 
+          {
+            type: Sequelize.BOOLEAN,
+            defaultValue: false 
+          }
+          );  
+      });
+     
   },
 
   down: (queryInterface, Sequelize) => {
@@ -41,18 +42,18 @@ module.exports = {
       Example:
       return queryInterface.dropTable('users');
     */
-   return queryInterface.removeColumn(
-      'Users',
-      'paymentStatus'
-    );
-    // return queryInterface.removeColumn(
-    //   'Users', // name of Source model
-    //   'teamId' // key we want to remove
-    // ).then(() => {
-    //   return queryInterface.removeColumn(
-    //     'Users',
-    //     'paymentStatus'
-    //   );
-    // });
+  //  return queryInterface.removeColumn(
+  //     'Users',
+  //     'paymentStatus'
+  //   );
+    return queryInterface.removeColumn(
+      'Users', // name of Source model
+      'teamId' // key we want to remove
+    ).then(() => {
+      return queryInterface.removeColumn(
+        'Users',
+        'paymentStatus'
+      );
+    });
   }
 };
